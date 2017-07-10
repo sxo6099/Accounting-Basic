bda8efa36b2e9283b3d05b9ff50236fb6af8e50b
Michael Gugino <guginomike@gmail.com>
2017-07-09 21:59:06 -0400

    This is our first test!

---
+++ Root/src/Database.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Michael Gugino on 7/9/2017.
+ */
+public class Database {
+    int num;
+
+    //This is a metaphore
+    public Database(int num){
+        this.num = num;
+    }
+}
